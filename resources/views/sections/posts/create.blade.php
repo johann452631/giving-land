8aaca7424d0ce8159e7450bc664654a04e34761b
alejoimbach1 <b_alejandro3010@outlook.com>
2024-06-04 13:47:48 -0500

    xd

--- resources/views/sections/posts/create.blade.php
+++ resources/views/sections/posts/create.blade.php
@@ -3,15 +3,9 @@
     <x-header/>
 
     {{-- main --}}
-    <div class="pt-24 pb-6">
+    <div class="pt-24 pb-6 screen-size">
         <div class="bg-gris-claro rounded p-10 max-w-2xl my-0 mx-auto">
-            <livewire:profile.edit.profile-img />
-            <hr>
-            <livewire:profile.edit.social-media />
-            <hr>
-            <livewire:profile.edit.contact-information />
-            <hr class="mb-4">
-            <a class="boton-base verde-blanco" href="{{route('profile.show',$username)}}">Regresar al perfil</a>
+            
         </div>
     </div>
     
