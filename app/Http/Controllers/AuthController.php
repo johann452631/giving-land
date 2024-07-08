42a5231c2ef4cd4077d9a0aed62332285cc20a7d
alejoimbach1 <b_alejandro3010@outlook.com>
2024-07-08 11:44:35 -0500

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use function Laravel\Prompts\error;
+
+class AuthController extends Controller
+{
+    //
+    public function login(LoginRequest $request)
+    {
+        if (!Auth::attempt($request->only(['email', 'password']))) {
+            $message = 'El correo electrónico o la contraseña son incorrectos.';
+            return response()->json([
+                'message' => $message,
+                'errors' => ['email' => $message]
+            ]);
+        }
+        // $request->session()->regenerate();
+        $user = User::where('email',$request->email)->firstOrFail();
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'user' => $user,
+            'accessToken' => $token,
+        ]);
+    }
+}
